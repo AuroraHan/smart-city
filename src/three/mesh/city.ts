c596264865ec834208ef64fbdd34664826fb46fd
guoxiaohan <guoxiaohan@pybctec.com>
2023-07-02 00:31:15 +0800

    城市高度渐变效果

--- src/three/mesh/city.ts
+++ src/three/mesh/city.ts
@@ -3,15 +3,19 @@ import * as THREE from "three";
 import scene from "@/three/scene";
 import { RGBELoader } from "three/examples/jsm/loaders/RGBELoader.js";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import modifyCityMaterial from "../modify/modifyCityMaterial";
 
 export default function createCity() {
   const gltfLoader = new GLTFLoader();
   gltfLoader.load("./model/city.glb", (gltf) => {
-    console.log(gltf);
     gltf.scene.traverse((item) => {
-      item.material = new THREE.MeshBasicMaterial({
-        color: new THREE.Color(0x00ffff),
-      });
+      if (item.type == "Mesh") {
+        const cityMaterial = new THREE.MeshBasicMaterial({
+          color: new THREE.Color(0x0c0e6f),
+        });
+        item.material = cityMaterial;
+        modifyCityMaterial(item);
+      }
     });
     scene.add(gltf.scene);
   });
