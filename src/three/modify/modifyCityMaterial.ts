e30acb79509f3f092c30a7efacbf4fec5a466b40
guoxiaohan <guoxiaohan@pybctec.com>
2023-07-02 17:55:07 +0800

    建筑光波，带状效果

--- src/three/modify/modifyCityMaterial.ts
+++ src/three/modify/modifyCityMaterial.ts
@@ -1,59 +1,160 @@
 import * as THREE from "three";
+import gsap from "gsap";
 export default function modifyCityMaterial(mesh: any) {
-  console.log(mesh);
-  mesh.geometry.computeBoundingBox();
-  let { min, max } = mesh.geometry.boundingBox;
-  let uHeight = max.y - min.y;
   mesh.material.onBeforeCompile = (shader: THREE.Shader) => {
     console.log(shader.vertexShader);
     console.log(shader.fragmentShader);
-    shader.uniforms.uTopColor = {
-      value: new THREE.Color("#aaaeff"),
-    };
-    shader.uniforms.uHeight = {
-      value: uHeight,
-    };
-
-    shader.vertexShader = shader.vertexShader.replace(
-      "#include <common>",
-      `
-        #include <common>
-        varying vec3 vPosition;
-        `
-    );
-
-    shader.vertexShader = shader.vertexShader.replace(
-      "#include <begin_vertex>",
-      `
-        #include <begin_vertex>
-          vPosition = position;
-          `
-    );
-
-    shader.fragmentShader = shader.fragmentShader.replace(
-      "#include <common>",
-      `
-        #include <common>
-
-        uniform vec3 uTopColor;
-        uniform float uHeight;
-        varying vec3 vPosition;
-        `
-    );
-
     shader.fragmentShader = shader.fragmentShader.replace(
       "#include <dithering_fragment>",
       `
-        #include <dithering_fragment>
-
-        vec4 distGradColor = gl_FragColor;
-
-        // 设置混合的百分比
-      float gradMix = (vPosition.y+uHeight/2.0)/uHeight;
-      // 计算出混合颜色
-      vec3 gradMixColor = mix(distGradColor.xyz,uTopColor,gradMix);
-      gl_FragColor = vec4(gradMixColor,1);
-        `
+          #include <dithering_fragment>
+          //#end#
+      `
     );
+    addGradColor(shader, mesh);
+    addSpread(shader);
+    addLightLine(shader);
   };
+}
+
+//添加渐变颜色
+export function addGradColor(shader: THREE.Shader, mesh: any) {
+  mesh.geometry.computeBoundingBox();
+  let { min, max } = mesh.geometry.boundingBox;
+  let uHeight = max.y - min.y;
+  shader.uniforms.uTopColor = {
+    value: new THREE.Color("#aaaeff"),
+  };
+  shader.uniforms.uHeight = {
+    value: uHeight,
+  };
+
+  shader.vertexShader = shader.vertexShader.replace(
+    "#include <common>",
+    `
+      #include <common>
+      varying vec3 vPosition;
+      `
+  );
+
+  shader.vertexShader = shader.vertexShader.replace(
+    "#include <begin_vertex>",
+    `
+      #include <begin_vertex>
+        vPosition = position;
+        `
+  );
+
+  shader.fragmentShader = shader.fragmentShader.replace(
+    "#include <common>",
+    `
+      #include <common>
+
+      uniform vec3 uTopColor;
+      uniform float uHeight;
+      varying vec3 vPosition;
+      `
+  );
+
+  shader.fragmentShader = shader.fragmentShader.replace(
+    "//#end#",
+    `
+      #include <dithering_fragment>
+
+      vec4 distGradColor = gl_FragColor;
+
+      // 设置混合的百分比
+    float gradMix = (vPosition.y+uHeight/2.0)/uHeight;
+    // 计算出混合颜色
+    vec3 gradMixColor = mix(distGradColor.xyz,uTopColor,gradMix);
+    gl_FragColor = vec4(gradMixColor,1);
+    //#end#
+      `
+  );
+}
+
+// 添加建筑材质光波扩散特效
+export function addSpread(
+  shader: THREE.Shader,
+  center = new THREE.Vector2(0, 0)
+) {
+  // 设置扩散的中心点
+  shader.uniforms.uSpreadCenter = { value: center };
+  //   扩散的时间
+  shader.uniforms.uSpreadTime = { value: -2000 };
+  //   设置条带的宽度
+  shader.uniforms.uSpreadWidth = { value: 40 };
+
+  shader.fragmentShader = shader.fragmentShader.replace(
+    "#include <common>",
+    `
+      #include <common>
+
+      uniform vec2 uSpreadCenter;
+      uniform float uSpreadTime;
+      uniform float uSpreadWidth;
+      `
+  );
+
+  shader.fragmentShader = shader.fragmentShader.replace(
+    "//#end#",
+    `
+     float spreadRadius = distance(vPosition.xz,uSpreadCenter);
+    //  扩散范围的函数
+    float spreadIndex = -(spreadRadius-uSpreadTime)*(spreadRadius-uSpreadTime)+uSpreadWidth;
+
+    if(spreadIndex>0.0){
+        gl_FragColor = mix(gl_FragColor,vec4(1,1,1,1),spreadIndex/uSpreadWidth);
+    }
+
+    //#end#
+    `
+  );
+
+  gsap.to(shader.uniforms.uSpreadTime, {
+    value: 800,
+    duration: 2,
+    ease: "none",
+    repeat: -1,
+  });
+}
+
+//带状扩散
+export function addLightLine(shader: THREE.Shader) {
+  //   扩散的时间
+  shader.uniforms.uLightLineTime = { value: -1500 };
+  //   设置条带的宽度
+  shader.uniforms.uLightLineWidth = { value: 200 };
+
+  shader.fragmentShader = shader.fragmentShader.replace(
+    "#include <common>",
+    `
+          #include <common>
+    
+          
+          uniform float uLightLineTime;
+          uniform float uLightLineWidth;
+          `
+  );
+
+  shader.fragmentShader = shader.fragmentShader.replace(
+    "//#end#",
+    `
+        float LightLineMix = -(vPosition.x+vPosition.z-uLightLineTime)*(vPosition.x+vPosition.z-uLightLineTime)+uLightLineWidth;
+    
+        if(LightLineMix>0.0){
+            gl_FragColor = mix(gl_FragColor,vec4(0.8,1.0,1.0,1),LightLineMix /uLightLineWidth);
+            
+        }
+    
+        //#end#
+        `
+  );
+
+  gsap.to(shader.uniforms.uLightLineTime, {
+    value: 1500,
+    duration: 5,
+    ease: "none",
+    repeat: -1,
+  });
 }
